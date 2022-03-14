62e79de2fe3e95ae044752d88ca03d429690ef8f
Valencia5 <mosesvalencia@Mosess-iMac.home>
2022-03-14 09:58:56 -0500

    js file

---
+++ js/js-with-HTML-practice.js
@@ -0,0 +1,6 @@
+var myName= "Moses";
+var age= 35;
+var isMonday = true;
+console.log(myName, age, isMonday);
+//Concatination below
+console.log (myName + "is awesome." + "i am" + age + "and it is" +isMonday)
