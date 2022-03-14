35758742e03d9458c52a8644055bcbe3efa0f055
Valencia5 <moses.valencia55@gmail.com>
2022-03-14 14:01:56 -0500

    another push

--- js/external.js
+++ js/external.js
@@ -4,17 +4,18 @@ alert("Welcome to my Website!");
 var userInput = prompt ('what is your favorite color?');
 console.log(userInput +  "is my favorite color too!");
 alert(userInput + " is my favorite color too!")
+
 var littlerMermaid = prompt("How many days are you renting Little Mermaid?")
 var  brotherBear = prompt( "How many days are you renting BrotherBear?")
-var hercules = prompt( "How many days are you renting BrotherBear?")
+var hercules = prompt( "How many days are you renting Hercules?")
 var totalCostOfRentals = (littlerMermaid * 3)+(brotherBear * 3) + (hercules *3)
-alert (totalCostOfRentals);
+alert ("The total cost of rentals is " + totalCostOfRentals);
 
 var Google= prompt ( "How many hours did you work for Google?");
 var Amazon= prompt ( "How many hours did you work for Amazon?");
 var Facebook= prompt ( "How many hours did you work for Facebook?");
 var totalWeeklyPay = (Google * 400) + (Amazon * 380) + (Facebook * 350);
-alert(totalWeeklyPay);
+alert("Your total pay is " + totalWeeklyPay);
 
 var classNotFull = true;
 var noScheduleConflict= true;
@@ -26,4 +27,3 @@ var notExpired = true;
 var premiumMember = true;
 var productOffer = moreThanTwo && notExpired || premiumMember
 alert ("You are eligible for product offer!");
-//changes
