e19840de0ad262434cf1e0104a907e71708841f6
Valencia5 <moses.valencia55@gmail.com>
2022-04-06 12:10:43 -0500

    lots

--- js/test-prep.js
+++ js/test-prep.js
@@ -1,5 +1,5 @@
 
-//adding arrays together
+//TODO:adding arrays together
 
 var passedNumbers = [1,'2',3,4,5];
 var result = 0;
@@ -12,3 +12,42 @@ function sumArr(input) {
     } return result;
 }
 sumArr(passedNumbers);
+
+
+
+//TODO: sort array and find length;
+
+// example input: [5,8,2,6,9,3,2];
+// expected output: [2,2,3,5,6,8,9] // length: 7
+// some possible test cases to think about:
+//     should be called ‘sortArr’
+// function should return an array
+// function should accept an array
+// BONUS thoughts
+// what would happen if you had negative numbers in your list
+// can you sort and then remove any duplicates (would it be more/less efficient to remove duplicates and then sort?)
+// if your array has non-numbers can you still sort the numbers? ex: input:
+// [3,“b”,8,5,9,true”,
+// 4,“xyz”] expected output: [3,4,5,8,9];
+
+
+function sortArr(array){
+    return ([array.sort() & array.length])
+}
+console.log( sortArr([5,8,2,6,9,3,2]))
+
+//TODO: what to do with neg numbers in the array;
+
+//TODO: how to remove duplicates;
+
+//TODO: how to remove non-numbers and convert string number to numbers.
+
+
+
+
+
+
+
+
+
+
