1ed7163121f07d09f8b01272ebe8e64affb1e499
Valencia5 <moses.valencia55@gmail.com>
2022-04-27 10:38:44 -0500

    first push

---
+++ js/jquery_exercises.js
@@ -0,0 +1,8 @@
+"use strict"
+console.log("hello World");
+
+//TODO: Add an alert to tell you the DOM has finished loading. Check for expected results.
+
+$(document).ready( function (){
+    alert( " The DOM has finished loading!!!")
+})
