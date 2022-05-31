46f2a09a74f7e27c5246d8d8749916e3d2df6e34
Valencia5 <moses.valencia55@gmail.com>
2022-05-31 09:05:40 -0500

    done

---
+++ js/promises.js
@@ -0,0 +1,25 @@
+"use strict"
+
+// const LastCommit = (user) => {
+//     return fetch(`https://api.github.com/users/valencia5/events`, {headers: {'Authorization': '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'}})
+//         .then(resp => resp.json())
+//         .then(data => {
+//             console.log(data);
+//             for (let event of data) {
+//                 if (event.type === "PushEvent") {
+//                     console.log(new Date(event.created_at));
+//                     break;
+//                 }
+//             }
+//         });
+// }
+//
+// LastCommit("moses-valencia");
+let getLastCommit=
+return fetch(`https://api.github.com/users/valencia5/events`, {headers: {'Authorization': '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'}}
+
+//todo: Create a function that accepts a GitHub username, and returns a promise that resolves returning
+// just the date of the last commit that user made. Reference the github api documentation to achieve this.
+
+
+
