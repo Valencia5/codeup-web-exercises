3f2db81395eb86652ef46f8b29b46b0f4ef1de8b
Valencia5 <moses.valencia55@gmail.com>
2022-05-31 09:16:08 -0500

    token removed

--- js/promises.js
+++ js/promises.js
@@ -16,7 +16,7 @@
 //
 // LastCommit("moses-valencia");
 let getLastCommit=
-return fetch(`https://api.github.com/users/valencia5/events`, {headers: {'Authorization': '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'}}
+return fetch(`https://api.github.com/users/valencia5/events`, {headers: { //Token goes here }}
 
 //todo: Create a function that accepts a GitHub username, and returns a promise that resolves returning
 // just the date of the last commit that user made. Reference the github api documentation to achieve this.
